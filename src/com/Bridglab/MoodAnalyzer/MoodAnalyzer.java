3cc10285f03cda8376b5ad5d1b0f7ac8349e3403
unknown <avinash.sankajji94@gmai.com>
2021-10-24 16:31:37 +0530

    Added:Analyze Happy

--- src/com/Bridglab/MoodAnalyzer/MoodAnalyzer.java
+++ src/com/Bridglab/MoodAnalyzer/MoodAnalyzer.java
@@ -1,7 +1,8 @@
 package com.Bridglab.MoodAnalyzer;
 
+
 public class MoodAnalyzer {
-    public String MoodAnalyzerr(String message) {
+       public String analyzerMood(String message) {
         if (message.contains("Sad"))
             return "SAD";
         else

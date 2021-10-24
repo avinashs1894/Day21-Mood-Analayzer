c8990f655526acb2a736d3b78e43b66ebcc1b29e
unknown <avinash.sankajji94@gmai.com>
2021-10-24 16:10:48 +0530

    Added:Create Object

---
+++ src/com/Bridglab/MoodAnalyzer/MoodAnalyzer.java
@@ -0,0 +1,11 @@
+package com.Bridglab.MoodAnalyzer;
+
+
+public class MoodAnalyzer {
+    public String MoodAnalyzerr(String message) {
+        if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
---
+++ src/com/Bridglab/MoodAnalyzer/MoodAnalyzerTest.java
@@ -0,0 +1,5 @@
+package com.Bridglab.MoodAnalyzer;
+
+public class MoodAnalyzerTest {
+
+}

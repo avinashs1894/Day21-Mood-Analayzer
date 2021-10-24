9e6d7313ca24f8f6491bd61e129e8a587b297741
unknown <avinash.sankajji94@gmai.com>
2021-10-24 16:34:26 +0530

    Added:Null Happy

--- src/com/Bridglab/MoodAnalyzer/MoodAnalyzer.java
+++ src/com/Bridglab/MoodAnalyzer/MoodAnalyzer.java
@@ -1,11 +1,24 @@
 package com.Bridglab.MoodAnalyzer;
 
-
 public class MoodAnalyzer {
-       public String analyzerMood(String message) {
-        if (message.contains("Sad"))
-            return "SAD";
-        else
+    private String message;
+
+    public MoodAnalyzer() {
+    }
+
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() {
+        try {
+
+            if (this.message.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
             return "HAPPY";
+        }
     }
 }
